8fa59cb6e0e8356f34c4319cea69cd0161ba96b1
Marcin Tanasiewicz <m@tanasiewicz.pl>
2019-03-24 17:25:30 +0100

    install JWT authentication pack

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,7 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     League\Tactician\Bundle\TacticianBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
