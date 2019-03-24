1d07d1711edd727595f0b0f7fd77913ef2e0dad7
Marcin Tanasiewicz <m@tanasiewicz.pl>
2019-03-24 17:45:06 +0100

    JWT configuration WIP and DB migration

---
+++ src/Shared/Model/Entity/RefreshToken.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Shared\Model\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use Gesdinet\JWTRefreshTokenBundle\Entity\AbstractRefreshToken;
+
+/**
+ * This class override Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken to have another table name.
+ *
+ * @ORM\Table("refresh_token")
+ */
+class RefreshToken extends AbstractRefreshToken
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        $this->id;
+    }
+}
