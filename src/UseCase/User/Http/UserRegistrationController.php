4f92857b340ca66adb298e244f2d2bb975347684
Marcin Tanasiewicz <m@tanasiewicz.pl>
2019-03-26 21:07:05 +0100

    drop refresh token entity

--- src/UseCase/User/Http/UserRegistrationController.php
+++ src/UseCase/User/Http/UserRegistrationController.php
@@ -6,12 +6,12 @@ namespace App\UseCase\User\Http;
 
 use App\Shared\Http\ApiControllerBase;
 use App\Shared\Model\Entity\Address;
-use App\Shared\Model\Entity\RefreshToken;
 use App\Shared\Model\Entity\User;
 use App\Shared\Model\Entity\Wallet;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Faker\Factory;
+use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -28,7 +28,8 @@ class UserRegistrationController extends AbstractController
         for ($i=0; $i<1000; $i++) {
             $user = new User(
                 $faker->email,
-                $faker->password
+                $faker->password,
+                $faker->name
             );
 
             $refreshToken = new RefreshToken();
@@ -36,20 +37,14 @@ class UserRegistrationController extends AbstractController
             $refreshToken->setUsername($user->getEmail());
             $refreshToken->setValid((new DateTime())->modify('+ 10 days'));
 
-            $wallet = new Wallet();
-            $wallet->setToken(uniqid());
-            $wallet->setUser($user);
-
             $address = new Address();
             $address->setFlat($faker->numberBetween(1,1000));
             $address->setStreet($faker->streetName);
             $address->setStreetNumber($faker->streetAddress);
             $user->addAddress($address);
-            $user->addWallet($wallet);
 
             $entityManager->persist($user);
             $entityManager->persist($refreshToken);
-            $entityManager->persist($wallet);
 
             if ($i % 50 === 0) {
                 $entityManager->flush();
@@ -58,7 +53,5 @@ class UserRegistrationController extends AbstractController
         }
 
         return new Response('yo');
-
-        return new User('email@example.com', 'Janusz Twardy');
     }
 }
